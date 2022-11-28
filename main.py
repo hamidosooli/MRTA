0aec8fcfc53ef9262b0a320ba0e55cdeaa3169dd
hamidosooli <hamid.osooli@gmail.com>
2022-11-28 16:19:44 -0500

    typo

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ from goal_allocation import make_span_calc, final_allocation
 
 
 make_span = {'FirstAids': 15, 'DebrisRemover': 30, 'OxygenCylinder': 20,
-             'Diffuser': 10, 'Manipulator': 45, 'FireExtinguisher': 35}
+             'Defuser': 10, 'Manipulator': 45, 'FireExtinguisher': 35}
 num_clusters = 5
 v1 = Victim(0, [2., 2.], make_span, ['DebrisRemover', 'OxygenCylinder'])
-v2 = Victim(1, [0., 13.], make_span, ['Diffuser', 'Manipulator'])
+v2 = Victim(1, [0., 13.], make_span, ['Defuser', 'Manipulator'])
 v3 = Victim(2, [7., 15.], make_span, ['DebrisRemover', 'FireExtinguisher'])
 v4 = Victim(3, [7., 1.], make_span, ['OxygenCylinder', 'Manipulator'])
 v5 = Victim(4, [7., 10.], make_span, ['FirstAids', 'DebrisRemover'])
 v6 = Victim(5, [6., 12.], make_span, ['Manipulator', 'FireExtinguisher'])
 v7 = Victim(6, [5., 7.], make_span, ['FirstAids', 'Manipulator'])
-v8 = Victim(7, [9., 19.], make_span, ['FirstAids', 'Diffuser'])
-v9 = Victim(8, [15., 14.], make_span, ['DebrisRemover', 'Diffuser'])
+v8 = Victim(7, [9., 19.], make_span, ['FirstAids', 'Defuser'])
+v9 = Victim(8, [15., 14.], make_span, ['DebrisRemover', 'Defuser'])
 v10 = Victim(9, [19., 11.], make_span, ['FirstAids', 'Manipulator'])
 
 victims = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10]
 
-r1 = Robot(0, [0., 0.], 0.2, [], num_clusters, [], ['Diffuser', 'DebrisRemover'])
+r1 = Robot(0, [0., 0.], 0.2, [], num_clusters, [], ['Defuser', 'DebrisRemover'])
 r2 = Robot(1, [0., 19.], 0.3, [], num_clusters, [], ['FirstAids', 'OxygenCylinder', 'Manipulator'])
 r3 = Robot(2, [19., 0.], 0.4, [], num_clusters, [], ['Manipulator'])
-r4 = Robot(3, [19., 19.], 0.3, [], num_clusters, [], ['FirstAids', 'Diffuser'])
+r4 = Robot(3, [19., 19.], 0.3, [], num_clusters, [], ['FirstAids', 'Defuser'])
 
 robots = [r1, r2, r3, r4]
 
